29d0ee7e500fecb80ae6166fa5c0c8015ddb685c
Cascallares Gabriel <git@github.com:gcascallares/tpDiscoProgBas2.git>
2018-09-30 20:06:58 -0300

    Home de Comercios

---
+++ comercioHome.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title>Nombre Pagina</title>
+		<link rel="stylesheet" href="./css/bootstrap.min.css">
+		<link rel="stylesheet" href="./css/estilosIndex.css">
+		<script src="./js/bootstrap.min.js"></script>
+		<script src="./js/jquery-3.3.1.min.js"></script>
+	</head>
+	<body>
+		<div class="container-fluid px-0">
+			<div class="header d-flex justify-content-between align-items-center">
+				<div class="logo"> ACA VA EL LOGO Y EL NOMBRE</div>
+					<div class="bar d-flex">
+						<div class="sesion">Bienvenido "Comercio"</div>
+						<div class="sesion"><a href="">Cerrar sesión</a></div>
+					</div>
+			</div>
+			<div class="header d-flex">
+				<div class="bar d-flex">
+					<div class="banner"><h1>Banner</h1></div>
+					<input type="submit" name="modificar" value="modificar">
+				</div>
+			</div>
+			<div class="title">
+				<h1>Mis Menus</h1>
+				<div class="container">
+					<div class="row">	 
+						<div class="col-md-4">
+							<div class="card">
+								<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+								<div class="card-body">
+									<h4 class="card-title">Menu 1</h4>
+									<p class="card-text">Descripcion</p>
+									<a href="#" class="btn btn-primary">Modificar</a>
+									<a href="#" class="btn btn-primary">Ver</a>
+								</div>
+							</div> 
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+								<div class="card-body">
+									<h4 class="card-title">Menu 2</h4>
+									<p class="card-text">Descripcion</p>
+									<a href="#" class="btn btn-primary">Modificar</a>
+									<a href="#" class="btn btn-primary">Ver</a>
+								</div>
+							</div> 
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+								<div class="card-body">
+									<h4 class="card-title">Menu 3</h4>
+									<p class="card-text">Descripcion</p>
+									<a href="#" class="btn btn-primary">Modificar</a>
+									<a href="#" class="btn btn-primary">Ver</a>
+								</div>
+							</div> 
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+								<div class="card-body">
+									<h4 class="card-title">Menu 4</h4>
+									<p class="card-text">Descripcion</p>
+									<a href="#" class="btn btn-primary">Modificar</a>
+									<a href="#" class="btn btn-primary">Ver</a>
+								</div>
+							</div> 
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+								<div class="card-body">
+									<h4 class="card-title">Menu 5</h4>
+									<p class="card-text">Descripcion</p>
+									<a href="#" class="btn btn-primary">Modificar</a>
+									<a href="#" class="btn btn-primary">Ver</a>
+								</div>
+							</div> 
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+								<div class="card-body">
+									<h4 class="card-title">Menu 6</h4>
+									<p class="card-text">Descripcion</p>
+									<a href="#" class="btn btn-primary">Modificar</a>
+									<a href="#" class="btn btn-primary">Ver</a>
+								</div>
+							</div> 
+						</div>
+					</div>
+				</div>
+				<a href="#" class="btn btn-primary">Ver Todos</a>
+			</div>
+		
+			<div class="title">
+					<h1>Mis Ofertas</h1>
+					<div class="container">
+						<div class="row">	 
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+									<div class="card-body">
+										<h4 class="card-title">Oferta 1</h4>
+										<p class="card-text">Descripcion</p>
+										<a href="#" class="btn btn-primary">Modificar</a>
+										<a href="#" class="btn btn-primary">Ver</a>
+									</div>
+								</div> 
+							</div>
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+									<div class="card-body">
+										<h4 class="card-title">Oferta 2</h4>
+										<p class="card-text">Descripcion</p>
+										<a href="#" class="btn btn-primary">Modificar</a>
+										<a href="#" class="btn btn-primary">Ver</a>
+									</div>
+								</div> 
+							</div>
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+									<div class="card-body">
+										<h4 class="card-title">Oferta 3</h4>
+										<p class="card-text">Descripcion</p>
+										<a href="#" class="btn btn-primary">Modificar</a>
+										<a href="#" class="btn btn-primary">Ver</a>
+									</div>
+								</div> 
+							</div>
+						</div>
+					</div>
+				<a href="#" class="btn btn-primary">Ver Todos</a>
+			</div>
+			<div class="title">
+					<h1>Registro de Activides</h1>
+					<div class="container">
+						<div class="row">
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+									<div class="card-body">
+										<h4 class="card-title">Ventas</h4>
+										<p class="card-text">Descripcion</p>
+									</div>
+								</div> 
+							</div>
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+									<div class="card-body">
+										<h4 class="card-title">Pagos</h4>
+										<p class="card-text">Descripcion</p>
+									</div>
+								</div> 
+							</div>
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src=".jpg" alt="Mi Imagen">
+									<div class="card-body">
+										<h4 class="card-title">Actividad</h4>
+										<p class="card-text">Descripcion</p>
+									</div>
+								</div> 
+							</div>
+						</div>
+					</div>
+					<a href="#" class="btn btn-primary">Ver Todos</a>
+			</div>
+		<div class="footer d-flex justify-content-center mt-4 pt-1">
+				INFORMACION SOBRE LA EMPRESA, FOOTER LINKS ETC...
+		</div>
+		</div>
+	</body>
+</html>
