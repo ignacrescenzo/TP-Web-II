7d9e10d49274f9494dec1ab36d207b9a3bad16e3
Minetti-C <31669218+Minetti-C@users.noreply.github.com>
2018-10-20 19:49:25 -0300

    Agrego 2 vistas en Restó

---
+++ resto/comercioHome2.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Restó | Comercios</title>
+    <meta name="description" content="Free Bootstrap Theme by uicookies.com">
+    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Pinyon+Script" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles-merged.css">
+    <link rel="stylesheet" href="css/style.min.css">
+
+  </head>
+  <body>
+    
+
+    <!-- Fixed navbar -->
+    
+    <nav class="navbar navbar-default navbar-fixed-top probootstrap-navbar">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html" title="uiCookies:FineOak">FineOak</a>
+        </div>
+
+        <div id="navbar-collapse" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#" data-nav-section="welcome">Bienvenido "Comercio"</a></li>
+            <li><a href="#" data-nav-section="specialties">Mis menús</a></li>
+            <li><a href="#" data-nav-section="menu">Mis ofertas</a></li>
+            <li><a href="#" data-nav-section="events">Estadísticas</a></li>
+            <li><a href="#" data-nav-section="contact">Cerrar Sesión</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <section class="flexslider" data-section="welcome">
+      <ul class="slides">
+        <li style="background-image: url(img/burger.jpg)" class="overlay" data-stellar-background-ratio="0.5">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-md-offset-2">
+                <div class="probootstrap-slider-text text-center probootstrap-animate probootstrap-heading">
+                  <h1 class="primary-heading">Bienvenido</h1>
+                  <h3 class="secondary-heading">a "Nombre Comercio"</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+
+
+<!-- OFERTAS DEL DIA -->
+
+    <section class="probootstrap-section">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center probootstrap-animate">
+            <div class="probootstrap-heading">
+              <h2 class="primary-heading">Ofertas</h2>
+              <h3 class="secondary-heading">del día</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-4 probootstrap-animate">
+            <div class="probootstrap-block-image">
+              <figure><img src="img/hamb7.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+              <div class="text">
+                <span class="date">June 29, 2017</span>
+                <h3><a href="#">Laboriosam Quod Dignissimos</a></h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+                <p class=""><a href="#" class="probootstrap-custom-link link-sm">Read More</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 probootstrap-animate">
+            <div class="probootstrap-block-image">
+              <figure><img src="img/hamb5.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+              <div class="text">
+                <span class="date">June 29, 2017</span>
+                <h3><a href="#">Laboriosam Quod Dignissimos</a></h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+                <p class=""><a href="#" class="probootstrap-custom-link link-sm">Read More</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 probootstrap-animate">
+            <div class="probootstrap-block-image">
+              <figure><img src="img/hamb4.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+              <div class="text">
+                <span class="date">June 29, 2017</span>
+                <h3><a href="#">Laboriosam Quod Dignissimos</a></h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+                <p class=""><a href="#" class="probootstrap-custom-link link-sm">Read More</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+<!-- MENÚ -->
+
+<section class="probootstrap-section probootstrap-bg-white">
+<div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center probootstrap-animate">
+            <div class="probootstrap-heading">
+              <h2 class="primary-heading">Mis</h2>
+              <h3 style="color: black;" class="secondary-heading">Menús</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <ul class="menus">
+              <li>
+                <figure class="image"><img src="img/hamb1.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">$22.99</span>
+                  <h3>Fried Potatoes with Garlic</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/hamb2.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">$22.99</span>
+                  <h3>Tuna Roast Source</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/hamb3.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">$22.99</span>
+                  <h3>Roast Beef (4 sticks)</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/hamb4.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">$22.99</span>
+                  <h3>Salted Fried Chicken</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <ul class="menus">
+              <li>
+                <figure class="image"><img src="img/hamb5.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">$22.99</span>
+                  <h3>Baked Potato Pizza</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/hamb6.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">$22.99</span>
+                  <h3>Fried Potatoes with Garlic</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/hamb7.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">$22.99</span>
+                  <h3>Salted Fried Chicken</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/hamb8.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">$22.99</span>
+                  <h3>Tuna Roast Source</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+<!-- FOOTER -->
+
+        <section class="probootstrap-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 probootstrap-animate">
+            <div class="probootstrap-footer-widget">
+              <h3><a href="#">Acerca de Restó® </a></h3>
+              <div class="row">
+                <div class="col-md-6">
+                  <a href="#"> Quiero ser Delivery</a>
+                </div>
+                <div class="col-md-6">
+                  <a href="#"> Quiero registrar mi Comercio</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 probootstrap-animate">
+            <div class="probootstrap-footer-widget">
+              <h3>Horarios</h3>
+              <div class="row">
+                <div class="col-md-4">
+                  <p>Todos los días <br> ¡las 24hs!</p>
+                </div>
+                <div class="col-md-4">
+                  <a href="#">Ayuda</a>
+                </div>
+                <div class="col-md-4">
+                  <a href="#">Medios de pago</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="probootstrap-copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <p class="copyright-text">&copy; 2018 <a href="#">Restó</a>. Todos los derechos reservados.
+          </div>
+          <div class="col-md-4">
+            <ul class="probootstrap-footer-social right">
+              <li><a href="#"><i class="icon-twitter"></i></a></li>
+              <li><a href="#"><i class="icon-facebook"></i></a></li>
+              <li><a href="#"><i class="icon-instagram"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <script src="js/scripts.min.js"></script>
+    <script src="js/custom.min.js"></script>
+
+  </body>
+  </html>
