ea7e4236e73b8f27c32a691e53210c2f19c42128
ignacrescenzo <ignadario97@gmail.com>
2018-10-20 17:44:07 -0300

    c

--- application/resources/js/comercioHome.js
+++ application/resources/js/comercioHome.js
@@ -3,15 +3,12 @@ $(document).ready(function(){
 //---Pasar Parámetros al Controlador -----
 
     $("#botonEliminar").click(function(){
-        var id = parseInt($("#menuId").text());
         //window.location = "menu/eliminar";
         $.ajax({
-            type:'POST',
-            url: 'menu/eliminar',
-            data: {'datos': id},
-            success:function(resp){
-                alert(resp);
-            }
+            type:"POST",
+            url: "menu/eliminar",
+            data: {"id": $("#menuId").text()},
+            success: window.location = "menu/eliminar"
         });
     });
 });
