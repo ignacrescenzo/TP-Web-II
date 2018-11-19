68d052f150087a65e89d3854ae2eb1b7a4abee26
Minetti-C <31669218+Minetti-C@users.noreply.github.com>
2018-11-19 20:42:45 -0300

    Vistas Delivery finalizadas!

--- application/view/deliveryHome.php
+++ application/view/deliveryHome.php
@@ -1,46 +1,116 @@
-	<?php
-		if(!isset($_SESSION["login"])){
-		echo "INICIA SESION WACHO";
+<?php
+  if(!isset($_SESSION["login"])){
+    echo "INISIA SESION WACHO";
         echo "<br>";
         echo "<a href='/login'>Iniciar sesion</a>";
         exit;
-	}
+  }
 ?>
+
 <!DOCTYPE html>
-<html lang="es">
-	<head>
-		<meta charset="UTF-8">
-		<title>Home Delivery</title>
-		<link rel="stylesheet" href="../application/resources/css/bootstrap.min.css">
-		<link rel="stylesheet" href="../application/resources/css/estilosIndex.css">
-		<link rel="stylesheet" href="../application/resources/css/comercioHome.css">
-		<script src="../application/resources/js/bootstrap.min.js"></script>
-		<script src="../application/resources/js/jquery-3.3.1.min.js"></script>
-        <script src="../application/resources/js/comercioHome.js"></script>
-	</head>
-	<body>
-		<div class="container-fluid px-0">
-			<div class="header d-flex justify-content-between align-items-center">
-				<div class="logo"> ACA VA EL LOGO Y EL NOMBRE</div>
-					<div class="bar d-flex">
-						<div class="sesion">Bienvenido "Delivey"</div>
-						<div class="sesion"><a href="/login/cerrarsesion">Cerrar sesión</a></div>
-					</div>
-			</div>
-			<div class="banner d-flex flex-column align-items-center">
-				<div><h3>Banner</h3></div>
-			</div>
-            <div class="text-center">
-				<h5>Para ver los pedidos disponibles entrá a trabajar! </h5>
-				<button><a href="/delivery/pedidosDisponibles"> Comenzar a trabajar </a></button>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Restó | Inicio</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Pinyon+Script" rel="stylesheet">
+
+    <!-- <link rel="stylesheet" href="../application/resources/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="../application/resources/css/styles-merged.css">
+    <link rel="stylesheet" href="../application/resources/css/style.min.css">
+    
+    <!-- <script src="../application/resources/js/jquery-3.3.1.min.js"></script>
+    <script src="../application/resources/js/bootstrap.min.js"></script> -->
+    <script src="../application/resources/js/scripts.min.js"></script>
+    <script src="../application/resources/js/custom.min.js"></script>
+    
+
+    <nav class="navbar navbar-default navbar-fixed-top probootstrap-navbar">
+      <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/" title="uiCookies:FineOak">FineOak</a>
+        </div>
+        <div id="navbar-collapse" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#" data-nav-section="welcome">Inicio</a></li>
+            <li><a onclick="location.href='/login/cerrarsesion'" >Cerrar sesión</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </head>
+  <body>
+
+<section class="probootstrap-section-bg overlay" style="background-image: url(../application/resources/img/hero_bg_1.jpg); height: 250px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center probootstrap-animate">
+            <div class="probootstrap-heading">
+              <h4> Para ver los pedidos disponibles entrá a trabajar! </h4>
+              <a href="/delivery/pedidosDisponibles" class='btn btn-primary'> Comenzar a trabajar </a> <br><br><br>
             </div>
+          </div>
+        </div>
+      </div>
 
-			
-				<!-- <a href="#" class="btn btn-primary mt-3">Ver Todos</a> -->
+		
 
-				<!-- <div class="text-center mb-3">
-				<a href="#" class="btn btn-primary mt-3">Ver Todos</a>
-				</div> -->
-		</div>
-	</body>
+<!-- FOOTER -->
+
+        <section class="probootstrap-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 probootstrap-animate">
+            <div class="probootstrap-footer-widget">
+              <h3><a href="#">Acerca de Restó® </a></h3>
+              <div class="row">
+                <div class="col-md-6">
+                  <a href="#"> Quiero ser Delivery</a>
+                </div>
+                <div class="col-md-6">
+                  <a href="/operadorComercio/registrarComercio"> Quiero registrar mi Comercio</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 probootstrap-animate">
+            <div class="probootstrap-footer-widget">
+              <h3>Horarios</h3>
+              <div class="row">
+                <div class="col-md-4">
+                  <p>Todos los días <br> ¡las 24hs!</p>
+                </div>
+                <div class="col-md-4">
+                  <a href="#">Ayuda</a>
+                </div>
+                <div class="col-md-4">
+                  <a href="#">Medios de pago</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="probootstrap-copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <p class="copyright-text">&copy; 2018 <a href="#">Restó</a>. Todos los derechos reservados.
+          </div>
+          <div class="col-md-4">
+            <ul class="probootstrap-footer-social right">
+              <li><a href="#"><i class="icon-twitter"></i></a></li>
+              <li><a href="#"><i class="icon-facebook"></i></a></li>
+              <li><a href="#"><i class="icon-instagram"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </body>
+
 </html>
