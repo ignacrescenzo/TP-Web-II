19a310e8c9843f5481139432fa7f2d2b5f049257
ignacio <ignadario97@gmail.com>
2018-11-20 18:00:13 -0300

    indent

--- application/view/mostrarEstadisticasComercio_view.php
+++ application/view/mostrarEstadisticasComercio_view.php
@@ -8,56 +8,58 @@
   ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-	<meta charset="UTF-8">
-	<title>Nombre Pagina</title>
+    <meta charset="UTF-8">
+    <title>Nombre Pagina</title>
     <link rel="stylesheet" href="../application/resources/css/bootstrap.min.css">
     <link rel="stylesheet" href="../application/resources/css/estilosIndex.css">
     <script src="../application/resources/js/bootstrap.min.js"></script>
     <script src="../application/resources/js/jquery-3.3.1.min.js"></script>
     <script src="../application/resources/js/comercioHome.js"></script>
 </head>
+
 <body>
-	<div class="container-fluid px-0">
-		<div class="header d-flex justify-content-between align-items-center">
-			<div class="logo"> ACA VA EL LOGO Y EL NOMBRE</div>
-			<div class="bar d-flex">
-						<div class="sesion"><a href="/login/cerrarsesion">Cerrar sesión</a></div>
-			</div>
-		</div>
-	
-		<br>
-		<h1>Estadisticas generales:</h1>
-<form method="POST" action="/operadorComercio/mostrarEstadisticas?idComercio=<?php echo $data ?>" enctype="application/x-www-form-urlencodes">
-		<label>Desde</label>
-		<input type="date" name="desde">
+    <div class="container-fluid px-0">
+        <div class="header d-flex justify-content-between align-items-center">
+            <div class="logo"> ACA VA EL LOGO Y EL NOMBRE</div>
+            <div class="bar d-flex">
+                <div class="sesion"><a href="/login/cerrarsesion">Cerrar sesión</a></div>
+            </div>
+        </div>
 
-		<label>Hasta</label>
-		<input type="date" name="hasta">
-		<input type="submit" name="buscar" value="buscar">
-</form>
+        <br>
+        <h1>Estadisticas generales:</h1>
+        <form method="POST" action="/operadorComercio/mostrarEstadisticas?idComercio=<?php echo $data ?>" enctype="application/x-www-form-urlencodes">
+            <label>Desde</label>
+            <input type="date" name="desde">
 
-<br>
-		<h3>Total ganancias:</h3>
-		<?php
+            <label>Hasta</label>
+            <input type="date" name="hasta">
+            <input type="submit" name="buscar" value="buscar">
+        </form>
+
+        <br>
+        <h3>Total ganancias:</h3>
+        <?php
 	if(mysqli_num_rows($data2)>0){
     while($rows=mysqli_fetch_assoc($data2)) { 		
 		echo "<h4>$".$rows['total']."</h4>";
     }
 }
 ?>
-<br>
-		<h3>Cobrado por el sistema:</h3>
-		<?php
+        <br>
+        <h3>Cobrado por el sistema:</h3>
+        <?php
 	if(mysqli_num_rows($data3)>0){
     while($rows=mysqli_fetch_assoc($data3)) { 		
 		echo "<h4>$".$rows['total']."</h4>";
     }
 }
 ?>
-<br>
-		<h3>Pagado a deliverys:</h3>
-		<?php
+        <br>
+        <h3>Pagado a deliverys:</h3>
+        <?php
 	if(mysqli_num_rows($data4)>0){
     while($rows=mysqli_fetch_assoc($data4)) { 		
 		echo "<h4>$".$rows['total']."</h4>";
@@ -66,6 +68,7 @@
 ?>
 
 
-	</div>
+    </div>
 </body>
+
 </html>
--- application/view/puntosDeVenta.php
+++ application/view/puntosDeVenta.php
@@ -9,60 +9,66 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Restó | Inicio</title>
-    
+
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Pinyon+Script" rel="stylesheet">
 
     <!-- <link rel="stylesheet" href="../application/resources/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="../application/resources/css/styles-merged.css">
     <link rel="stylesheet" href="../application/resources/css/style.min.css">
-    
+
     <!-- <script src="../application/resources/js/jquery-3.3.1.min.js"></script>
     <script src="../application/resources/js/bootstrap.min.js"></script> -->
-    <script src="../application/resources/js/scripts.min.js"></script>
-    <script src="../application/resources/js/custom.min.js"></script>
-    
+    <script src="../application/resources/js/scripts.min.js">
+    </script>
+    <script src="../application/resources/js/custom.min.js">
+    </script>
+
 
     <nav class="navbar navbar-default navbar-fixed-top probootstrap-navbar">
-      <div class="container">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="/" title="uiCookies:FineOak">FineOak</a>
-        </div>
-        <?php
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/" title="uiCookies:FineOak">FineOak</a>
+            </div>
+            <?php
             $idComercio=$data2;
             ?>
-        <div id="navbar-collapse" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#" data-nav-section="welcome">Inicio</a></li>
-            <li><a onclick="location.href=<?php echo "'/OperadorComercio/crearPuntoDeVenta?idComercio=".$idComercio."'" ?>" >Crear punto de venta</a></li>
-            <li><a  onclick="location.href=<?php echo "'/OperadorComercio/estadisticas?idComercio=".$idComercio."'" ?>" >Estadisticas</a></li>
-            <li><a onclick="location.href='/login/cerrarsesion'" >Cerrar sesión</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </head>
-  <body>
-
-<section class="probootstrap-section-bg overlay" style="background-image: url(../application/resources/img/hero_bg_1.jpg); height: 300px;">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 text-center probootstrap-animate">
-            <div class="probootstrap-heading">
-              <h3 class="secondary-heading" style="color: black; font-size: 30px;">Selecciona un punto de venta para gestionar!</h3>
+            <div id="navbar-collapse" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#" data-nav-section="welcome">Inicio</a></li>
+                    <li><a style="cursor:pointer" onclick="location.href=<?php echo "'/OperadorComercio/crearPuntoDeVenta?idComercio=".$idComercio."'" ?>">Crear
+                            punto de venta</a></li>
+                    <li><a style="cursor:pointer" onclick="location.href=<?php echo "'/OperadorComercio/estadisticas?idComercio=".$idComercio."'" ?>">Estadisticas</a></li>
+                    <li><a style="cursor:pointer" onclick="location.href='/login/cerrarsesion'">Cerrar sesión</a></li>
+                </ul>
             </div>
-          </div>
         </div>
-      </div>
+    </nav>
+</head>
+
+<body>
+
+    <section class="probootstrap-section-bg overlay" style="background-image: url(../application/resources/img/hero_bg_1.jpg); height: 300px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center probootstrap-animate">
+                    <div class="probootstrap-heading">
+                        <h3 class="secondary-heading" style="color: black; font-size: 30px;">Selecciona un punto de
+                            venta para gestionar!</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
-<section class="probootstrap-section">
-      <div class="container">
-        <div class="row">
-            <?php 
+    <section class="probootstrap-section">
+        <div class="container">
+            <div class="row">
+                <?php 
             while($rows=mysqli_fetch_assoc($data)){ 
                     
                     echo "
@@ -87,63 +93,63 @@
             </div>
         </div>
     </section>
- 
-     </body>
+
+</body>
 <!-- FOOTER -->
 
-        <section class="probootstrap-footer">
-      <div class="container">
+<section class="probootstrap-footer">
+    <div class="container">
         <div class="row">
-          <div class="col-md-6 probootstrap-animate">
-            <div class="probootstrap-footer-widget">
-              <h3><a href="#">Acerca de Restó® </a></h3>
-              <div class="row">
-                <div class="col-md-6">
-                  <a href="#"> Quiero ser Delivery</a>
+            <div class="col-md-6 probootstrap-animate">
+                <div class="probootstrap-footer-widget">
+                    <h3><a href="#">Acerca de Restó® </a></h3>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="#"> Quiero ser Delivery</a>
+                        </div>
+                        <div class="col-md-6">
+                            <a href="/operadorComercio/registrarComercio"> Quiero registrar mi Comercio</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-6">
-                  <a href="/operadorComercio/registrarComercio"> Quiero registrar mi Comercio</a>
-                </div>
-              </div>
             </div>
-          </div>
-          <div class="col-md-6 probootstrap-animate">
-            <div class="probootstrap-footer-widget">
-              <h3>Horarios</h3>
-              <div class="row">
-                <div class="col-md-4">
-                  <p>Todos los días <br> ¡las 24hs!</p>
+            <div class="col-md-6 probootstrap-animate">
+                <div class="probootstrap-footer-widget">
+                    <h3>Horarios</h3>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <p>Todos los días <br> ¡las 24hs!</p>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#">Ayuda</a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#">Medios de pago</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-4">
-                  <a href="#">Ayuda</a>
-                </div>
-                <div class="col-md-4">
-                  <a href="#">Medios de pago</a>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </section>
+    </div>
+</section>
 
-    <section class="probootstrap-copyright">
-      <div class="container">
+<section class="probootstrap-copyright">
+    <div class="container">
         <div class="row">
-          <div class="col-md-8">
-            <p class="copyright-text">&copy; 2018 <a href="#">Restó</a>. Todos los derechos reservados.
-          </div>
-          <div class="col-md-4">
-            <ul class="probootstrap-footer-social right">
-              <li><a href="#"><i class="icon-twitter"></i></a></li>
-              <li><a href="#"><i class="icon-facebook"></i></a></li>
-              <li><a href="#"><i class="icon-instagram"></i></a></li>
-            </ul>
-          </div>
+            <div class="col-md-8">
+                <p class="copyright-text">&copy; 2018 <a href="#">Restó</a>. Todos los derechos reservados.
+            </div>
+            <div class="col-md-4">
+                <ul class="probootstrap-footer-social right">
+                    <li><a href="#"><i class="icon-twitter"></i></a></li>
+                    <li><a href="#"><i class="icon-facebook"></i></a></li>
+                    <li><a href="#"><i class="icon-instagram"></i></a></li>
+                </ul>
+            </div>
         </div>
-      </div>
-    </section>
+    </div>
+</section>
 
-  </body>
+</body>
 
 </html>
